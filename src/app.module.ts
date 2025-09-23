d2824322bb0246a2d022b86c157c934f90b2245f
FSALVA157 <fsalva157@hotmail.com>
2025-09-22 22:32:06 -0300

    first state

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT'), 10),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // Note: set to false in production
+      }),
+      inject: [ConfigService],
+    }),
+    // Other modules can be imported here
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
